b62f36d5c4d5f45bb0775ae707a8bfa6001f838b
theoigen <oigenZ@icloud.com>
2019-02-23 18:47:56 +0200

    changed eslint

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const path = require('path')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const busboyBodyParser = require('busboy-body-parser');
 const mongoose = require('mongoose');
@@ -9,15 +9,15 @@ const cloudinary = require("cloudinary");
 const config = require("./config");
 const passport = require('passport');
 
-const app = express()
+const app = express();
 
 const publicPath = path.join(__dirname, 'public');
 
 app.use(cors());
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static(publicPath))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(publicPath));
 app.use(passport.initialize());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,11 +33,11 @@ const connectOptions = {
     useCreateIndex: true
 };
 mongoose.connect(config.mongo_url, connectOptions)
-.then((x) => {
-    console.log("Mongo database connected " + mongoose.connection);
-    app.listen(config.port, function () { console.log('Server is ready\n' + publicPath); });
-})
-.catch((err) => console.log("ERROR: " + err.message));
+    .then((x) => {
+        console.log("Mongo database connected " + mongoose.connection);
+        app.listen(config.port, function () { console.log('Server is ready\n' + publicPath); });
+    })
+    .catch((err) => console.log("ERROR: " + err.message));
 
 const authRouter = require("./routes/auth");
 
@@ -51,4 +51,4 @@ function enableCors(req, res, next) {
 }
 app.use("/auth", enableCors, authRouter);
 
-module.exports = app
+module.exports = app;
